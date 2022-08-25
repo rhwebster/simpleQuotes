75746adaf1529f9c5ad63a7d511aa7f864f289fb
Ryan Webster <ryan.h.webster@gmail.com>
2022-08-25 16:19:13 -0700

    basic routes

--- server.js
+++ server.js
@@ -15,6 +15,44 @@ const fetchQuote = async () => {
 
 const server = http.createServer((req, res) => {
 
+    function redirectTo(urlPath) {
+        res.statusCode = 302;
+        res.setHeader('Location', urlPath);
+        return res.end();
+    }
+
+    req.on('end', async () => {
+
+        if (req.method === "GET" && req.url === "/") {
+            const htmlFile = fs.readFileSync("./vews/index.html", "utf-8");
+            const resBody = htmlFile
+                .replace(/#{quote}/g, `Click below for your first quote`);
+            
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.write(resBody);
+            return res.end();
+        }
+
+        if (req.method === "POST" && req.url === "/quote") {
+            const quote = await getQuote();
+            const text = quote.text;
+            const author = quote.author;
+
+            if (quote) {
+                const htmlFile = fs.readFileSync("./views/index.html", "utf-8");
+                let reqBody = htmlFile
+                    .replace(/#{quote}/g, `"${text}" - ${author}`);
+
+                    res.statusCode = 200;
+                    res.setHeader("Content-Type", "text/html");
+                    res.write(reqBody);
+                    return res.end();
+            } else {
+                return redirectTo('/');
+            }
+        }
+    })
 })
 
 const port = 5000;
